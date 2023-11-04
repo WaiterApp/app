dbda005fc2b579c2fd4e4722acd932ab85107d6c
MichaelGB96 <MichaelGB96@users.noreply.github.com>
2023-11-05 00:19:06 +0100

    agregar funcionalidad al boton continuar sin iniciar sesion

--- app/src/main/java/es/ifp/waiterapp/InicioSesion.java
+++ app/src/main/java/es/ifp/waiterapp/InicioSesion.java
@@ -2,13 +2,42 @@ package es.ifp.waiterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InicioSesion extends AppCompatActivity {
+
+    // Variables
+    private EditText box_usuario, box_contrasena;
+    private Button btn_inicioS, btn_registro, btn_continuarSinS;
+    private Intent pasarPantalla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+
+        // Relacionar las variables con los elementos del UI
+        box_usuario = findViewById(R.id.box_usuario_iniciosesion);
+        box_contrasena = findViewById(R.id.box_contrasena_iniciosesion);
+
+        btn_inicioS = findViewById(R.id.button_incia_iniciosesion);
+        btn_registro = findViewById(R.id.button_registro_iniciosesion);
+        btn_continuarSinS = findViewById(R.id.button_noinicio_iniciosesion);
+
+        // Ir a la actividad CodigoRestaurante si se elige continuar sin iniciar sesión
+        btn_continuarSinS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pasarPantalla = new Intent(InicioSesion.this, CodigoRestaurante.class);
+                finish();
+                startActivity(pasarPantalla);
+            }
+        });
+
     }
+
 }
